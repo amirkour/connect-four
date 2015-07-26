940062a0aa93c995c36b558a8cca3e845e54d59c
amir kouretchian <amirsgarbunk@gmail.com>
2015-07-25 20:25:16 -0700

    introducing connect-four-db-app and initial implementation of ApplicationDAOPlayerType

---
+++ connect-four-db-app/src/org/amirk/games/connectfour/db/app/ApplicationDAOPlayerType.java
@@ -0,0 +1,27 @@
+
+package org.amirk.games.connectfour.db.app;
+
+import java.util.List;
+import org.amirk.games.connectfour.db.DAOPlayerType;
+import org.amirk.games.connectfour.entities.PlayerType;
+import org.hibernate.Transaction;
+
+public class ApplicationDAOPlayerType extends DAOPlayerType {
+    
+    @Override
+    public List<PlayerType> getList(){
+        List<PlayerType> list = null;
+        Transaction tx = null;
+        
+        try{
+            tx = this.sessionFactory.getCurrentSession().beginTransaction();
+            list = super.getList();
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+            throw e;
+        }
+        
+        return list;
+    }
+}
