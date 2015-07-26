24b4ab928fb6889827384e0f7917626a3244ce0b
amir kouretchian <amirsgarbunk@gmail.com>
2015-07-25 20:10:48 -0700

    a few smoke tests to ensure hashcode and equality builders work as expected

--- connect-four-entities/test/org/amirk/games/connectfour/entities/test/PlayerTypeTest.java
+++ connect-four-entities/test/org/amirk/games/connectfour/entities/test/PlayerTypeTest.java
@@ -1,19 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.amirk.games.connectfour.entities.test;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.amirk.games.connectfour.entities.*;
 
-/**
- *
- * @author AmirKouretchian
- */
 public class PlayerTypeTest {
     
     @Test
-    public void doesStuff(){}
+    public void equalityShouldWork(){
+        PlayerType one = new PlayerType();
+        one.setId(1);
+        one.setName("hi");
+
+        PlayerType two = new PlayerType();
+        two.setId(1);
+        two.setName("hi");
+
+        assertEquals(one,two);
+
+        two.setId(2);
+        assertNotEquals(one,two);
+
+        two.setId(1);
+        two.setName("foo");
+
+        assertNotEquals(one,two);
+
+        two.setName(null);
+        assertNotEquals(one,two);
+    }
+
+    @Test
+    public void hashCodeShouldWork(){
+        PlayerType one = new PlayerType();
+        one.setId(1);
+        one.setName("hi");
+
+        PlayerType two = new PlayerType();
+        two.setId(1);
+        two.setName("hi");
+
+        assertEquals(one.hashCode(),two.hashCode());
+
+        two.setId(2);
+        assertNotEquals(one.hashCode(),two.hashCode());
+
+        two.setId(1);
+        two.setName("foo");
+
+        assertNotEquals(one.hashCode(), two.hashCode());
+
+        two.setName(null);
+        assertNotEquals(one.hashCode(), two.hashCode());
+    }
 }
