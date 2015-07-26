c3409d1922fde658c62f9df974cbb0674c4205c5
amir kouretchian <amirsgarbunk@gmail.com>
2015-07-25 20:08:26 -0700

    adding PlayerTypeTest skeleton and dependencies

---
+++ connect-four-entities/test/org/amirk/games/connectfour/entities/test/PlayerTypeTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.amirk.games.connectfour.entities.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author AmirKouretchian
+ */
+public class PlayerTypeTest {
+    
+    @Test
+    public void doesStuff(){}
+}
