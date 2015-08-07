4d2af77686ed6c9facc63fb07874e3c81c8253f7
amir kouretchian <amirsgarbunk@gmail.com>
2015-08-07 12:55:11 -0700

    adding barebone game class - need to test some git config changes for a sec ...

---
+++ connect-four-entities/src/org/amirk/games/connectfour/entities/Game.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.amirk.games.connectfour.entities;
+
+/**
+ *
+ * @author foo
+ */
+public class Game {
+    
+}
